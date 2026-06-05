ae40fc698f3e18076f4bf3c3a3c37714be3d132e
AlexandreG83 <alexandre.guillamon83@gmail.com>
2026-06-05 15:30:35 +0200

    remove a comma

--- sql_join/11-course-registration-count.sql
+++ sql_join/11-course-registration-count.sql
@@ -2,5 +2,5 @@ SELECT courses.title AS course_title,
     COUNT(registrations.student_id) AS number_of_registration
 FROM courses
 LEFT JOIN registrations ON registrations.course_id = courses.id
-GROUP BY courses.id,
+GROUP BY courses.id
 ORDER BY number_of_registration DESC, course_title ASC;
